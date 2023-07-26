ace6522f4b52b833fd5c1abb3756ec013b1b23fb
Jordan Gudim <137233322+Jpgudim@users.noreply.github.com>
2023-07-25 21:26:22 -0500

    Project Complete (for now)
    patch notes:
    - removed print statements like "The dealer drives a card..." to reduce clutter in output.
    
    - changed some basic formatting for output print statements
    
    - removed unneseccary if statement for when the player chooses to hit. This now makes the while loop un-nested and removes an unneseccary variable ("decision"). Now program just uses "turn" to store input string and determine if the player wants to hit.
    
    issues:
    - none at the moment.

--- Blackjack 2.0.py
+++ Blackjack 2.0.py
@@ -55,31 +55,30 @@ else:
 n = 2
 m = 2
 
-if turn == "hit":
-    decision = "hit"
-    while decision == "hit":
-        print ("The dealer gives you a card...")
-        deal_player()
-        print ("The dealer gave you a " + str(player_hand[n])) 
-        n+=1
+while turn == "hit":
+    deal_player()
+    print ("The dealer gave you a " + str(player_hand[n])) 
+    n+=1
+    player_score = get_player_score()
+    print ("Your score is now " + str(player_score))
+    if player_score > 21 and "ace" in player_hand and deck["ace"] != 1:
+        deck["ace"] = 1
         player_score = get_player_score()
-        print ("Your score is now " + str(player_score))
-        if player_score > 21 and "ace" in player_hand and deck["ace"] != 1:
-            deck["ace"] = 1
-            player_score = get_player_score()
-            print ("Luckily you have an Ace! Your score is now " + str(player_score))
-            continue
-        if player_score > 21:
-            print ("You went over 21! You lose")
-            print ("The dealer's second card was a " + str(dealer_hand[1]))
-            print ("The dealer had a score of " + str(get_dealer_score()))
-            exit()
-        if player_score == 21:
-            print ("You have 21! You win!")
-            exit()
-        else:
-            decision = input("What would you like to do? (hit or stand) ")
-            print ()
+        print ("Luckily you have an Ace! Your score is now " + str(player_score))
+        continue
+    if player_score > 21:
+        print()
+        print ("You went over 21! You lose")
+        print ("The dealer's second card was a " + str(dealer_hand[1]))
+        print ("The dealer had a score of " + str(get_dealer_score()))
+        exit()
+    if player_score == 21:
+        print()
+        print ("You have 21! You win!")
+        exit()
+    else:
+        decision = input("What would you like to do? (hit or stand) ")
+        print ()
 
 
 print ("The dealer's second card is a " + str(dealer_hand[1]))
@@ -89,7 +88,6 @@ print ("The dealer has a score of "+ str(dealer_score))
 deck["ace"] = 11
 
 while dealer_score < 17 and player_score < 21:
-    print ("The dealer draws a card...")
     deal_dealer()
     print ("The dealer drew a " + str(dealer_hand[m]))
     m += 1
@@ -98,7 +96,7 @@ while dealer_score < 17 and player_score < 21:
     if dealer_score >21 and "ace" in dealer_hand and deck["ace"] != 1:
         deck["ace"] = 1
         dealer_score = get_dealer_score()
-        print ("The dealer has an ace, so not the dealer's score is " + str(dealer_score))
+        print ("The dealer has an ace, so now the dealer's score is " + str(dealer_score))
         continue
 if dealer_score == 21:
     print ()
@@ -110,10 +108,13 @@ if dealer_score > 21:
     exit()
 
 if dealer_score > player_score:
+    print()
     print ("The dealer has a higher score. The dealer wins.")
 if player_score > dealer_score:
+    print()
     print ("You have a higher score than the dealer! You win!")
 if player_score == dealer_score:
+    print()
     print ("You have the same score as the dealer. That's a push!")
         
 
