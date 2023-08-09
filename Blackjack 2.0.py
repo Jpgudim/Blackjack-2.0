63b7b179b4f3562877e807fbaa69fa1a5b5c6657
Jordan Gudim <137233322+Jpgudim@users.noreply.github.com>
2023-08-08 23:39:31 -0500

    Added time intervals between print statements!
    - added 1 second breaks in between print statements to make the output more smooth
    
    
    - noticed a problem when the dealer has more that one ace. The dealer has a score of 22, not 12.

--- Blackjack 2.0.py
+++ Blackjack 2.0.py
@@ -4,6 +4,7 @@
 #To solve this, I essentially have to start from scratch#
 
 import random
+import time
 
 deck = {"ace":11, "2":2, "3":3, "4":4, "5":5, "5":5, "6":6, "7":7, "8":8, "9":9, "10":10, "Jack":10, "Queen":10, "King":10}
 player_hand = []
@@ -37,7 +38,9 @@ deal_player()
 deal_dealer()
 
 print ("You are dealt a " + str(player_hand[0]) + " and a " + str(player_hand[1]))
+time.sleep(1)
 print ("The dealer was dealt a " + str(dealer_hand[0]) + " and another card that is face down")
+time.sleep(1)
 player_score = get_player_score()
 dealer_score = get_dealer_score()
 if player_score == 21:
@@ -57,26 +60,33 @@ m = 2
 
 while turn == "hit":
     deal_player()
+    time.sleep(1)
     print ("The dealer gave you a " + str(player_hand[n])) 
     n+=1
     player_score = get_player_score()
+    time.sleep(1)
     print ("Your score is now " + str(player_score))
     if player_score > 21 and "ace" in player_hand and deck["ace"] != 1:
         deck["ace"] = 1
         player_score = get_player_score()
+        time.sleep(1)
         print ("Luckily you have an Ace! Your score is now " + str(player_score))
         continue
     if player_score > 21:
         print()
         print ("You went over 21! You lose")
+        time.sleep(1)
         print ("The dealer's second card was a " + str(dealer_hand[1]))
+        time.sleep(1)
         print ("The dealer had a score of " + str(get_dealer_score()))
         exit()
     if player_score == 21:
         print()
+        time.sleep(1)
         print ("You have 21! You win!")
         exit()
     else:
+        time.sleep(1)
         turn = input("What would you like to do? (hit or stand) ")
         print ()
 
@@ -89,32 +99,40 @@ deck["ace"] = 11
 
 while dealer_score < 17 and player_score < 21:
     deal_dealer()
+    time.sleep(1)
     print ("The dealer drew a " + str(dealer_hand[m]))
     m += 1
     dealer_score = get_dealer_score()
+    time.sleep(1)
     print ("The dealer now has a score of " + str(dealer_score))
     if dealer_score >21 and "ace" in dealer_hand and deck["ace"] != 1:
         deck["ace"] = 1
         dealer_score = get_dealer_score()
+        time.sleep(1)
         print ("The dealer has an ace, so now the dealer's score is " + str(dealer_score))
         continue
 if dealer_score == 21:
     print ()
+    time.sleep(1)
     print ("The dealer has 21! The dealer wins.")
     exit()
 if dealer_score > 21:
     print ()
+    time.sleep(1)
     print ("The dealer busts! You win!")
     exit()
 
 if dealer_score > player_score:
     print()
+    time.sleep(1)
     print ("The dealer has a higher score. The dealer wins.")
 if player_score > dealer_score:
     print()
+    time.sleep(1)
     print ("You have a higher score than the dealer! You win!")
 if player_score == dealer_score:
     print()
+    time.sleep(1)
     print ("You have the same score as the dealer. That's a push!")
         
 
